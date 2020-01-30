5dfedf47a03bf717c91e0ba16e2ef59dd1c1c86d
MarkalS <jyysherry91713@163.com>
2020-01-30 18:32:36 +0800

    Add files via upload

---
+++ run_analysis.R
@@ -0,0 +1,49 @@
+# Course Project
+# Human_Activity_Recognition_Using_Smartphones
+setwd("./UCI HAR Dataset/")  # All data is in this folder
+library(dplyr)
+library(tidyr)
+
+# 1.Reading Datasets
+## The Activity Labels and Features
+activeLabel <- read.table("activity_labels.txt", stringsAsFactors = F)
+featuresTB <- read.table("features.txt", stringsAsFactors = F)
+
+## The Train Data
+trainSet <- read.table("./train/X_train.txt", stringsAsFactors = F)
+trainLabel <- read.table("./train/y_train.txt", stringsAsFactors = F)
+trainSubject <- read.table("./train/subject_train.txt", stringsAsFactors = F)
+
+## The Test Data
+testSet <- read.table("./test/X_test.txt", stringsAsFactors = F)
+testLabel <- read.table("./test/y_test.txt", stringsAsFactors = F)
+testSubject <- read.table("./test/subject_test.txt", stringsAsFactors = F)
+
+
+# 2.Merges the training and the test sets
+testSet$Group <- "test"
+trainSet$Group <- "train"
+dbCombine <- bind_rows(trainSet, testSet)
+subCombine <- bind_rows(trainSubject, testSubject)
+lbCombine <- bind_rows(trainLabel, testLabel)
+dbCombine$Subjects <- subCombine$V1
+dbCombine$Label_Num <- lbCombine$V1
+
+
+# 3.Extracts only the measurements on the mean and standard deviation 
+#   for each measurement
+colnames(dbCombine)[1:561] <- featuresTB$V2
+colnames(dbCombine) <- gsub("\\(\\)", "", colnames(dbCombine))
+selectedNm <- grep("mean|std", featuresTB$V2)
+dbMeaStd <- dbCombine[, c(selectedNm, 562:564)]
+
+
+# 4.Uses descriptive activity names to name the activities in the data set
+# 5.Appropriately labels the data set with descriptive variable names
+dbMeaStd <- dbMeaStd %>% 
+    mutate(ActivityLabel = activeLabel$V2[match(Label_Num, activeLabel$V1)]) %>% 
+    select(-Label_Num) %>% 
+    gather("Feature_StatsType", "StatsResults", -Group, -Subjects, -ActivityLabel) %>% 
+    separate(Feature_StatsType, c("Feature", "StatsType"))
+    # print
+
